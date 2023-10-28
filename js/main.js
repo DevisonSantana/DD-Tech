685cce3e398af27ad689f209c2a54c739ae9e099
Deividson Omedio <deividsonomedio@outlook.com>
2023-10-28 01:40:32 -0300

    hahaha

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var banner_img = document.getElementById("banner_img");
+img = document.getElementsByClassName("banner_img");
+var products_img = document.getElementById("products_img");
+var x = banner_img.clientWidth
+var cont = 0
+setInterval(rolagemAutomatico, 3000)
+
+function rolagemAutomatico() {
+    banner_img.scrollLeft += x
+    cont++
+    if (cont === 3) {
+        banner_img.scrollLeft += -(3 * x)
+        cont = 0
+    }
+}
+
+function LeftBanner() {
+
+    banner_img.scrollLeft += -x
+
+}
+
+function RightBanner() {
+
+    banner_img.scrollLeft += x
+
+}
+
+function LeftNovos() {
+    products_img.scrollLeft = -150
+}
+
+function RightNovos() {
+    products_img.scrollLeft = 150
+}
+
