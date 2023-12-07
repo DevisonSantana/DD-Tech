939901962876a9aaa32f31f68da9e43455e7c817
DevisonSantana <devisonsantana@hotmail.com>
2023-12-07 17:45:11 -0300

    UPDATE >> JavaScript Login > Implementações interativas

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,6 +1,7 @@
 // BOTÃO DE RETORNO PARA LOGIN
 let btnLogin = document.querySelector('main div button')
-btnLogin.addEventListener('click', ()=>{
+btnLogin.addEventListener('click', (e)=>{
+    e.preventDefault()
     window.location.href = 'login.html'
 })
 
@@ -9,17 +10,21 @@ let emailInput = document.getElementById('email-input')
 let emailHelper = document.getElementById('email-helper')
 
 // Function Email Input
+let emailValid = false
+
 emailInput.addEventListener('change', function(e){
-    let valor = e.target.value;
+    let valor = e.target.value
     if (valor.includes("@") && valor.includes(".com")){
         emailInput.classList.add('correct')
         emailInput.classList.remove('error')
         emailHelper.classList.remove('visible')
+        emailValid = true
     } else {
         emailInput.classList.add('error')
         emailInput.classList.remove('correct')
         emailHelper.classList.add('visible')
-        emailHelper.innerHTML = "Email inválido, por favor digite um email válido."
+        emailHelper.innerText = "Email inválido, por favor digite um email válido."
+        emailValid = false
     }
 })
 
@@ -50,6 +55,8 @@ let confirmaSenhaInput = document.getElementById('confirma-senha-input')
 let confirmaSenhaHelper = document.getElementById('confirma-senha-helper')
 
 // Function Confirma Senha Input
+let senhaValid = false
+
 function funcConfirmaSenha(e){
     valor = e.target.value
     if(valor.length < 5){
@@ -57,15 +64,18 @@ function funcConfirmaSenha(e){
         confirmaSenhaInput.classList.remove('correct')
         confirmaSenhaHelper.classList.add('visible')
         confirmaSenhaHelper.innerText = 'Senha muito curta, tente mais caracteres'
+        senhaValid = false
     } else if(valor == senhaInput.value){
         confirmaSenhaInput.classList.add('correct')
         confirmaSenhaInput.classList.remove('error')
         confirmaSenhaHelper.classList.remove('visible')
+        senhaValid = true
     } else {
         confirmaSenhaInput.classList.add('error')
         confirmaSenhaInput.classList.remove('correct')
         confirmaSenhaHelper.classList.add('visible')
         confirmaSenhaHelper.innerText = 'As senhas não coincidem.'
+        senhaValid = false
     }
 }
 
@@ -77,17 +87,21 @@ let nameInput = document.getElementById('name-input')
 let nameHelper = document.getElementById('name-helper')
 
 // Function Nome Input
+let nameValid = false
+
 nameInput.addEventListener('change', (e)=>{
     let valor = e.target.value
     if(valor.length > 3){
         nameInput.classList.remove('error')
         nameInput.classList.add('correct')
         nameHelper.classList.remove('visible')
+        nameValid = true
     } else {
         nameInput.classList.add('error')
         nameInput.classList.remove('correct')
         nameHelper.classList.add('visible')
-        nameHelper.innerHTML = "Nome deve ter mais do que 3 caracteres."
+        nameHelper.innerText = "Nome deve ter mais do que 3 caracteres."
+        nameValid = false
     }
 })
 
@@ -96,17 +110,21 @@ let sobrenomeInput = document.getElementById('sobrenome-input')
 let sobrenomeHelper = document.getElementById('sobrenome-helper')
 
 // Function Sobrenome Input
+let sobrenomeValid = false
+
 sobrenomeInput.addEventListener('change', (e)=>{
     let valor = e.target.value
     if(valor.length > 4){
         sobrenomeInput.classList.remove('error')
         sobrenomeInput.classList.add('correct')
         sobrenomeHelper.classList.remove('visible')
+        sobrenomeValid = true
     } else {
         sobrenomeInput.classList.add('error')
         sobrenomeInput.classList.remove('correct')
         sobrenomeHelper.classList.add('visible')
-        sobrenomeHelper.innerHTML = "Sobrenome deve ter mais do que 4 caracteres."
+        sobrenomeHelper.innerText = "Sobrenome deve ter mais do que 4 caracteres."
+        sobrenomeValid = false
     }
 })
 
@@ -114,10 +132,153 @@ sobrenomeInput.addEventListener('change', (e)=>{
 let cpfInput = document.getElementById('cpf-input')
 let cpfHelper = document.getElementById('cpf-helper')
 
+// Function CPF Input
+let cpfValid = false
+
+cpfInput.addEventListener('change', (e)=>{
+    let valor = e.target.value
+    if(valor.length == 11){
+        cpfInput.classList.add('correct')
+        cpfInput.classList.remove('error')
+        cpfHelper.classList.remove('visible')
+        cpfValid = true
+    } else {
+        cpfInput.classList.remove('correct')
+        cpfInput.classList.add('error')
+        cpfHelper.classList.add('visible')
+        cpfHelper.innerText = "CPF inválido, por favor preencha corretamente."
+        cpfValid = false
+    }
+})
+
 // ENDEREÇO INPUT
 let enderecoInput = document.getElementById('endereco-input')
 let enderecoHelper = document.getElementById('endereco-helper')
 
+// Function Endereço Input
+let enderecoValid = false
+
+enderecoInput.addEventListener('change', (e)=>{
+    let valor = e.target.value
+    if(valor.length < 5){
+        enderecoInput.classList.add('error')
+        enderecoInput.classList.remove('correct')
+        enderecoHelper.classList.add('visible')
+        enderecoHelper.innerText = "Endereço inválido, por favor preencha corretamente."
+        enderecoValid = false
+    } else {
+        enderecoInput.classList.add('correct')
+        enderecoInput.classList.remove('error')
+        enderecoHelper.classList.remove('visible')
+        enderecoValid = true
+    }
+})
+
 // CIDADE INPUT
 let cidadeInput = document.getElementById('cidade-input')
 let cidadeHelper = document.getElementById('cidade-helper')
+
+// Function Cidade Input
+let cidadeValid = false
+
+cidadeInput.addEventListener('change', (e)=>{
+    let valor = e.target.value
+    if(valor.length < 5){
+        cidadeInput.classList.add('error')
+        cidadeInput.classList.remove('correct')
+        cidadeHelper.classList.add('visible')
+        cidadeHelper.innerText = "Cidade incorreta, por favor preencha corretamente."
+        cidadeValid = false
+    } else {
+        cidadeInput.classList.add('correct')
+        cidadeInput.classList.remove('error')
+        cidadeHelper.classList.remove('visible')
+        cidadeValid = true
+    }
+})
+
+// CEP INPUT
+let cepInput = document.getElementById('cep-input')
+let cepHelper = document.getElementById('cep-helper')
+
+// Function CEP Input
+let cepValid = false
+
+cepInput.addEventListener('change', (e)=>{
+    let valor = e.target.value
+    if(valor.length == 8){
+        cepInput.classList.add('correct')
+        cepInput.classList.remove('error')
+        cepHelper.classList.remove('visible')
+        cepValid = true
+    } else if(valor.includes('-') || valor.includes('.')){
+        cepInput.classList.add('error')
+        cepInput.classList.remove('correct')
+        cepHelper.classList.add('visible')
+        cepHelper.innerText = "Por favor, insira o CEP sem traços ou pontos."
+        cepValid = false
+    } else {
+        cepInput.classList.add('error')
+        cepInput.classList.remove('correct')
+        cepHelper.classList.add('visible')
+        cepHelper.innerText = "CEP inválido, preencha os dados corretamente."
+        cepValid = false
+    }
+})
+
+// CELULAR INPUT
+let celularInput = document.getElementById('celular-input')
+let celularHelper = document.getElementById('celular-helper')
+
+// Function Celular Input
+let celularValid = false
+
+celularInput.addEventListener('change', (e)=>{
+    let valor = e.target.value
+    if(valor.length == 14 && valor.includes('+')){
+        celularInput.classList.add('correct')
+        celularInput.classList.remove('error')
+        celularHelper.classList.remove('visible')
+        celularValid = true
+    }else if(valor.includes(' ')){
+        celularInput.classList.add('error')
+        celularInput.classList.remove('correct')
+        celularHelper.classList.add('visible')
+        celularHelper.innerText = "Por favor, preencha os dados sem espaçamentos."
+        celularValid = false
+    } else if(!valor.includes('+')){
+        celularInput.classList.add('error')
+        celularInput.classList.remove('correct')
+        celularHelper.classList.add('visible')
+        celularHelper.innerText = "Por favor, informe seu DDI."
+        celularValid = false
+    } else if(valor.length == 11){
+        celularInput.classList.add('error')
+        celularInput.classList.remove('correct')
+        celularHelper.classList.add('visible')
+        celularHelper.innerText = "Por favor, informe seu DDD, sem parenteses."
+        celularValid = false
+    } else {
+        celularInput.classList.add('error')
+        celularInput.classList.remove('correct')
+        celularHelper.classList.add('visible')
+        celularHelper.innerText = "Dados incorretos, por favor insira um numero válido."
+        celularValid = false
+    }
+})
+
+// BOTÃO SUBMIT
+let submit = document.getElementById('btn-criar-conta')
+submit.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(emailValid == true && senhaValid === true && nameValid == true  
+        && sobrenomeValid == true && cpfValid == true 
+        && enderecoValid == true && cidadeValid == true 
+        && cepValid == true && celularValid == true)
+    {
+        alert("Seus dados foram cadastrados com sucesso!!")
+        window.location.href = 'login.html'
+    } else {
+        alert("Não foi possível concluir o cadastro 😰 verifique seus dados e tente novamente.")
+    }
+})
--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+// VALIDAR EMAIL
 let emailInput = document.getElementById('email-input')
 let emailHelper = document.getElementById('email-helper')
 
@@ -17,6 +18,7 @@ emailInput.addEventListener('change', function(e){
     }
 })
 
+// VALIDAR SENHA
 let senhaInput = document.getElementById('senha-input')
 let senhaHelper = document.getElementById('senha-helper')
 
@@ -26,11 +28,13 @@ function senha(e){
             senhaInput.classList.add('error')
             senhaInput.classList.remove('correct')
             senhaHelper.classList.add('visible')
-            senhaHelper.innerText = "Senha incorreta."
+            senhaHelper.innerText = "Senha deve conter mais do que 4 caracteres."
+
         } else {
             senhaInput.classList.add('correct')
             senhaInput.classList.remove('error')
             senhaHelper.classList.remove('visible')
+            
         }
     }
 
@@ -38,7 +42,8 @@ senhaInput.addEventListener('focus', senha)
 
 senhaInput.addEventListener('blur', senha)
 
-let cadastro = document.querySelector('.btn-criar-conta')
+// ENCAMINHAR PARA PAGINA CADASTRO
+let cadastro = document.querySelector('#btn-criar-conta')
 
 cadastro.addEventListener('click', (e)=>{
     
@@ -51,31 +56,38 @@ cadastro.addEventListener('click', (e)=>{
 
 let usuario = [
     {
-        email: "devison@email.com",
+        user: "devison",
+        email: "devison@proz.com",
         senha: "admin"
     },
     {
-        email: "emerson@email.com",
+        user: "emerson",
+        email: "emerson@proz.com",
         senha: "admin"
     },
     {
-        email: "deividson@email.com",
+        user: "deividson",
+        email: "deividson@proz.com",
         senha: "admin"
     },
     {
-        email: "gabriel@email.com",
+        user: "gabriel",
+        email: "gabriel@proz.com",
         senha: "admin"
+    },
+    {
+        user: "alvaro",
+        email: "alvinhu@proz.com",
+        senha: "nota10"
     }
 ]
 
-let submit = document.querySelector('.btn-submit')
+let submit = document.querySelector('#btn-submit')
 
 submit.addEventListener('click', function(e){
-
+    e.preventDefault()
     for (let i = 0; i < usuario.length; i++){
-
         let valid = false
-
         if (usuario[i].email == emailInput.value && usuario[i].senha == senhaInput.value){
             valid = true
             window.location.href = 'usuario.html'
@@ -86,12 +98,8 @@ submit.addEventListener('click', function(e){
             senhaInput.classList.remove('error')
             senhaHelper.classList.remove('visible')
             break
-
-        } else {
-            e.preventDefault()
-            valid = false
-            
         }
+
         if(valid == false){
             emailInput.classList.add('error')
             emailInput.classList.remove('correct')
@@ -99,7 +107,64 @@ submit.addEventListener('click', function(e){
             emailHelper.innerHTML = "Email ou senha incorretos, tente novamente."
             senhaInput.classList.add('error')
             senhaInput.classList.remove('correct')
-
         }
+
+    }
+})
+
+// MOSTRANDO RECUPERAÇÃO DE SENHA
+let esqueciSenha = document.querySelector('.forgot-pwd')
+let blocoEsqueciSenha = document.getElementById('menu-esqueci-senha')
+
+esqueciSenha.addEventListener('click', ()=>{
+    blocoEsqueciSenha.classList.toggle('not-visible')
+    
+})
+
+// ESQUECI MINHA SENHA
+let showUsers = document.getElementsByClassName('spn-blue')
+let listaUsers = document.getElementById('lista-users')
+
+
+for(let i = 0; i < usuario.length; i++){
+    let li = document.createElement('li')
+    li.innerHTML = usuario[i].user
+    listaUsers.appendChild(li)
+}
+
+showUsers[1].addEventListener('click', ()=>{
+    listaUsers.classList.toggle('not-visible')
+    listaUsers.classList.toggle('visible')
+})
+
+
+// RECUPERAÇÃO DE SENHA
+let forgotSenhaInput = document.getElementById('forgot-pass-input')
+let forgotSenhaHelper = document.getElementById('forgot-pass-helper')
+let btnForgotPass = document.getElementById('btn-forgot-pass')
+
+
+btnForgotPass.addEventListener('click', (e)=>{
+    e.preventDefault()
+    for(i = 0; i < usuario.length; i++){
+        let valid = false
+
+        if (usuario[i].user == forgotSenhaInput.value){
+            valid = true
+            forgotSenhaHelper.innerText = `Email: ${usuario[i].email}
+            Senha: ${usuario[i].senha}`
+            forgotSenhaHelper.classList.add('forgot-pass-style')
+            forgotSenhaHelper.classList.remove('helper-text')
+            forgotSenhaHelper.classList.remove('visible')
+            break
+        }
+
+        if(valid == false){
+            forgotSenhaHelper.classList.add('helper-text')
+            forgotSenhaHelper.classList.remove('forgot-pass-style')
+            forgotSenhaHelper.classList.add('visible')
+            forgotSenhaHelper.innerText = "Usuario não encontrado, tente novamente."
+        }
+
     }
 })
