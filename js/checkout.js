16317ec540321a62320c289c779c90848cd14c20
anbuyyy <anbuyyy9@gmail.com>
2024-01-07 19:21:51 -0300

    Realizando primeiras alterações no checlout.JS

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,54 @@
-let valorQuantidade = document.getElementById("input.quantidade")
-console.log("valorQuantidade")
+//Capturando os elementos DOM(Variaveis)
+let valorQuantidade = document.getElementById("quantidade") // 1
+let valorSoma = document.getElementById("soma") //8.000,00
+let btnAdicionarPruduto01 = document.getElementById("adicionar") //botao adicionar + 1
+let btnSubtrairProduto01 = document.getElementById("subtrair")
+let qtdProduto01 = document.getElementById("quantidade")
 
-valorQuantidade = "red"
+console.log("btnSubtrair")
+
+console.log(valorSoma, valorQuantidade, btnAdicionarPruduto01)
+
+//Objeto-01
+let subtotalInfo = {
+    quantidade:1,
+    valor: 15.000,
+};
+
+//objeto-02
+let subtotalInfo1 = {
+    quantidade:1,
+    valor: 15.000,
+};
+
+
+//Definir a manipular elementos(funções)
+
+function atualizarSubtotal(){
+    valorQuantidade.innerText = subtotalInfo + "item";
+    valorSoma.innerText = subtotalInfo.valor;
+
+    
+
+}
+
+atualizarSubtotal()
+
+function adicionarUm(){
+    qtdProduto01.value = Number(qtdProduto01.value) + 1 //Adicionar um  
+
+    atualizarSubtotal()
+}
+
+function subtrairUm(){
+    qtdProduto01.value = Number(qtdProduto01.value) - 1; // Subtrair 1 em vez de adicionar 1
+
+    atualizarSubtotal
+
+}
+
+
+
+//Definir quando devem ser manipulados os elementos (eventos)
+btnAdicionarPruduto01.addEventListener("click", adicionarUm)
+btnSubtrairProduto01.addEventListener("click", subtrairUm)
